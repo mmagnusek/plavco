3f6556009be28160d46b0a8deb75fe8f5b59ffb0
Martin Magnusek <magnusekm@gmail.com>
2026-02-06 22:01:07 +0100

    Force new host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,4 +42,8 @@ class ApplicationController < ActionController::Base
   def current_ability
     @current_ability ||= Ability.new(current_user)
   end
+
+  def redirect_to_new_host
+    redirect_to "https://www.rozpiska.cz" if Rails.env.production? && request.host.include?("herokuapp.com")
+  end
 end
